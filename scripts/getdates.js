231578b04e54fc800d7bf984c9b41537e99bdd77
JoaquinECB <jcaceresbello@byui.edu>
2025-07-19 01:03:58 -0500

    Finish changes

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,19 +1,5 @@
-// Get the current year
 const currentYear = new Date().getFullYear();
 document.getElementById('currentyear').textContent = currentYear;
 
-// Get the last modified date
 const lastModified = document.lastModified;
 document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
-
-// Select the footer paragraphs
-const footer = document.querySelector('footer');
-if (footer) {
-    const paragraphs = footer.querySelectorAll('p');
-    if (paragraphs.length > 0) {
-        paragraphs[0].textContent = `© ${currentYear}`;
-    }
-    if (paragraphs.length > 1) {
-        paragraphs[1].textContent = `Last Modified: ${lastModified}`;
-    }
-}
